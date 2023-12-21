7e169eac4de879ec9d3f9a0a18bbe998bb97a964
yahya42k <yahya.abdurrahim@gmail.com>
2023-12-21 08:18:13 +0300

    Intro completed

--- part-1/main.cpp
+++ part-1/main.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
-#include"tester.h"
+
 using namespace std;
 
 int main() {
 	
-	int passed = 0;
-	passed += testSuite(5, 3, 125);
-	passed += testSuite(5, -3, 0.008);
-	cout << passed << " TEST PASSED " << endl;
+
 	
 
 	return 0;
