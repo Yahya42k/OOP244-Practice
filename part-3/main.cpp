98211a0840e6c032b78e14fbcb6aa68dc45a2c90
yahya42k <yahya.abdurrahim@gmail.com>
2023-12-23 04:47:47 +0300

    extra

--- part-3/main.cpp
+++ part-3/main.cpp
@@ -2,22 +2,10 @@
 
 using namespace std;
 
-class num {
-	int x;
-public:
-	num() {
-		x = 5;
-	}
 
-	void display() const {
-		cout << this << endl;
-		cout << this->x << endl;
-	}
-};
 
 int main() {
-	num five;
-	five.display();
+	
 
 	return 0;
 }
